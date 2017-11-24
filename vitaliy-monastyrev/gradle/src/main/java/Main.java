a72e0d33a438a64cc57cc5d1d79e5fc0f920be51
MrSandmanRUS <vit34-95@mail.ru>
2017-11-24 04:02:18 +0300

    Added project

---
+++ vitaliy-monastyrev/gradle/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(3, 2));
+        System.out.println(calculator.minus(3, 2));
+        System.out.println(calculator.multiply(3, 2));
+        System.out.println(calculator.divide(4, 2));
+    }
+}
